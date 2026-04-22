8073081a5014552cf40085c582b67dc6ad28cc1c
kkiarie <kiariekevin22@gmail.com>
2026-04-22 15:10:50 +0300

    [ADD] Open erp sh for odoo application

--- tests/openodoo.spec.ts
+++ tests/openodoo.spec.ts
@@ -12,10 +12,15 @@ test ('open odoo staging', async({page}) => {
 
     // Click my projects drop down
     await page.getByRole('link', {name: 'Sign in'}).click()
-    await page.getByRole('button', {name: 'kiarie kevin'}).click()
-    await page.getByRole('menuitem', {name: 'My Project(s)'}).click()
 
-    // Expect ponty-erp to be visible
+    // Enter odoo.sh dashboard
+    await page.locator('#login_field').fill('kiariekevin22@gmail.com')
+    await page.locator('#password').fill('$kingara120')
+    await page.click('input[type="submit"][value="Sign in"]');
     await expect(page.getByRole('link', {name: 'ponty-erp'})).toBeVisible()
+
+    //Open odoo-staging application
+    await page.getByRole('link',{name: 'Open'}).click()
+    await expect(page).toHaveURL('https://www.odoo.sh/project/ponty-erp')
 
 })
