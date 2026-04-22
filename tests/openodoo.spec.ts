9a6450910a259e3932880b83a9c6a52c81590045
kkiarie <kiariekevin22@gmail.com>
2026-04-22 12:22:02 +0300

    [ADD] Added open odoo staging test

--- tests/openodoo.spec.ts
+++ tests/openodoo.spec.ts
@@ -1,8 +1,21 @@
 import { test, expect } from "@playwright/test"
 
-test ('has title', async({page}) => {
+// test ('has title', async({page}) => {
+//     await page.goto('https://www.odoo.sh/')
+
+//     // Expect odoo.sh page title
+//     await expect(page).toHaveTitle("The Odoo Cloud Platform | Official Odoo Hosting")
+// })
+
+test ('open odoo staging', async({page}) => {
     await page.goto('https://www.odoo.sh/')
 
-    // Expect odoo.sh page title
-    await expect(page).toHaveTitle("The Odoo Cloud Platform | Official Odoo Hosting")
+    // Click my projects drop down
+    await page.getByRole('link', {name: 'Sign in'}).click()
+    await page.getByRole('button', {name: 'kiarie kevin'}).click()
+    await page.getByRole('menuitem', {name: 'My Project(s)'}).click()
+
+    // Expect ponty-erp to be visible
+    await expect(page.getByRole('link', {name: 'ponty-erp'})).toBeVisible()
+
 })
