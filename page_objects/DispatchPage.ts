7ddbf8c33bfddec61063a93c61f7f552aed27781
kkiarie <kiariekevin22@gmail.com>
2026-04-27 12:14:47 +0300

    [ADD] Increased test timeout on the openodoo.spec.ts test file

--- page_objects/DispatchPage.ts
+++ page_objects/DispatchPage.ts
@@ -8,12 +8,17 @@ export class DispatchPage extends Basepage{
 
     async openDispatch(){
         // const page1 = await this.page.waitForEvent('popup');
-        console.log(this.page.url())
+        // console.log(this.page.url())
         await this.page.getByRole('option', { name: 'Dispatch' }).click();
-        console.log(this.page.url())
+        // await this.page.waitForTimeout(15000);
+        const groupRows = this.page.locator('tr.o_group_header');
+
+        await expect(groupRows.first()).toBeVisible({ timeout: 20000 });
+        // console.log(this.page.url())
         // await this.page.goto('https://ponty-erp-staging-31243847.dev.odoo.com/odoo/action-624');
+      
         const januaryGroup = this.page.locator('tr:has-text("January 2023")');
-        await expect(januaryGroup).toBeVisible();
+        await expect(januaryGroup).toBeVisible({timeout: 15000});
         await januaryGroup.click();
     }
 
--- tests/openodoo.spec.ts
+++ tests/openodoo.spec.ts
@@ -3,16 +3,13 @@ import { OdooShPage } from "../page_objects/OdooShPage"
 import { LandinPage } from "../page_objects/LandingPage"
 import { DispatchPage } from "../page_objects/DispatchPage"
 
-// let odoosh: OdooShPage
-// let landingpage: LandinPage
-// let dispatchpage: DispatchPage
+
 
 
 
 
 test ('open odoo staging', async({page}) => {
-    // odoosh = new OdooShPage(page)
-    // await odoosh.visit()
+    test.setTimeout(60000)
     const landingpage = new LandinPage(page)
     const stagingpage = await landingpage.open_landing_page();
 
