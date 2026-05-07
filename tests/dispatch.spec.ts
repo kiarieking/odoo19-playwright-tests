f3c4b670807fb11fd1d31cf6bd446e34964e3035
kiarieking <kiariekevin22@gmail.com>
2026-05-07 21:37:09 +0300

    [UPDATE] Verify dispatch actions using status bar

--- tests/dispatch.spec.ts
+++ tests/dispatch.spec.ts
@@ -5,59 +5,62 @@ import { DispatchPage } from "../page_objects/DispatchPage"
 
 test.describe.serial('Dispatch workflow', () => {
 
-    test.beforeEach(async({page}) => {
+    // test.beforeEach(async({page}) => {
         
+    // })
+
+    test ('Reset cancelled dispatch to draft', async({page}) => {
+        test.setTimeout(60000)
+        const landingpage = new LandinPage(page)
+        const stagingpage = await landingpage.open_landing_page();
+
+        const dispatchpage = new DispatchPage(stagingpage)
+
+        await dispatchpage.openDispatch()
+
+        await dispatchpage.openDispatchStatus('Cancelled (')
+        
+        await dispatchpage.reset_btn.click()
+
+        // await expect(dispatchpage.confirm_dispatch).toBeVisible()
+        await expect(dispatchpage.quotation_status_bar).toHaveAttribute('aria-checked', 'true')
+
     })
 
-    // test ('Reset cancelled dispatch to draft', async({page}) => {
-    //     test.setTimeout(60000)
-    //     const landingpage = new LandinPage(page)
-    //     const stagingpage = await landingpage.open_landing_page();
+    test ('Approve dispatch', async({page}) => {
+        test.setTimeout(60000)
+        const landingpage = new LandinPage(page)
+        const stagingpage = await landingpage.open_landing_page();
 
-    //     const dispatchpage = new DispatchPage(stagingpage)
+        const dispatchpage = new DispatchPage(stagingpage)
 
-    //     await dispatchpage.openDispatch()
+        await dispatchpage.openDispatch()
 
-    //     await dispatchpage.openDispatchStatus('Cancelled (')
+        await dispatchpage.openDispatchStatus('Delivered (')
         
-    //     await dispatchpage.reset_btn.click()
+        await dispatchpage.approve_dispatch.click()
 
-    //     await expect(dispatchpage.confirm_dispatch).toBeVisible()
-
-    // })
-
-    // test ('Approve dispatch', async({page}) => {
-    //     test.setTimeout(60000)
-    //     const landingpage = new LandinPage(page)
-    //     const stagingpage = await landingpage.open_landing_page();
-
-    //     const dispatchpage = new DispatchPage(stagingpage)
-
-    //     await dispatchpage.openDispatch()
-
-    //     await dispatchpage.openDispatchStatus('Delivered (')
-        
-    //     await dispatchpage.approve_dispatch.click()
-
-    //     await expect(dispatchpage.post_dispatch).toBeVisible()
-    // })
+        // await expect(dispatchpage.post_dispatch).toBeVisible()
+        await expect(dispatchpage.to_approve_status_bar).toHaveAttribute('aria-checked', 'true')
+    })
 
 
-    // test ('Confirm dispatch', async({page}) => {
-    //     test.setTimeout(60000)
-    //     const landingpage = new LandinPage(page)
-    //     const stagingpage = await landingpage.open_landing_page();
+    test ('Confirm dispatch', async({page}) => {
+        test.setTimeout(60000)
+        const landingpage = new LandinPage(page)
+        const stagingpage = await landingpage.open_landing_page();
 
-    //     const dispatchpage = new DispatchPage(stagingpage)
+        const dispatchpage = new DispatchPage(stagingpage)
 
-    //     await dispatchpage.openDispatch()
+        await dispatchpage.openDispatch()
 
-    //     await dispatchpage.openDispatchStatus('Quotation (')
+        await dispatchpage.openDispatchStatus('Quotation (')
 
-    //     await dispatchpage.confirm_dispatch.click()
+        await dispatchpage.confirm_dispatch.click()
 
-    //     await expect(dispatchpage.dispatch_action).toBeVisible()
-    // })
+        // await expect(dispatchpage.dispatch_action).toBeVisible()
+        await expect(dispatchpage.admin_ops_status_bar).toHaveAttribute('aria-checked', 'true')
+    })
 
     test ('Confirm delivered', async({page}) => {
         test.setTimeout(60000)
@@ -73,7 +76,7 @@ test.describe.serial('Dispatch workflow', () => {
         await dispatchpage.confirm_delivered.click()
 
         // await expect(dispatchpage.approve_dispatch).toBeVisible()
-        await expect(dispatchpage.status_bar).toHaveAttribute('aria-checked', 'true')
+        await expect(dispatchpage.delivered_status_bar).toHaveAttribute('aria-checked', 'true')
 
     })
 
