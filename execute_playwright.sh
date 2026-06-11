542fe889e73c69f3625ca396816292be149351be
kkiarie <kiariekevin22@gmail.com>
2026-06-11 12:29:08 +0300

    Updated node installation

--- execute_playwright.sh
+++ execute_playwright.sh
@@ -16,7 +16,7 @@ setup_odoo19tests(){
     apt-get install -y curl
     curl -fsSL https://deb.nodesource.com/setup_24.x | bash -
     apt-get install -y nodejs
-    nsolid -v
+    node -v
 
     npm install
 
